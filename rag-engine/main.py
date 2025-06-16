83e06a8a959ef953b9c8d4cf2a34f470a0c6ba98
vvaselin <87738472+vvaselin@users.noreply.github.com>
2025-06-16 09:34:38 +0900

    CROS調整

--- rag-engine/main.py
+++ rag-engine/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8888"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
